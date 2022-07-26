0effa80150bba12c5fb46c8ce42a3e0b9c37ffd3
aokarski <a.okarski@smartrecruiters.com>
2022-07-26 19:08:26 +0200

    init commit for the spring boot init generated project

---
+++ src/main/java/it/highersoft/smarthosttask/SmarthostTaskApplication.java
@@ -0,0 +1,13 @@
+package it.highersoft.smarthosttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarthostTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarthostTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/highersoft/smarthosttask/SmarthostTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package it.highersoft.smarthosttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarthostTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
